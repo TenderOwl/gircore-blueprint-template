79055eb4dcdb440a886c962a34a1c6c65c9f65e0
Andrey Maksimov <andrey@tenderowl.com>
2024-04-22 19:33:45 +0300

    Add DependencyInjection with ServiceCollection

--- template/__APP_NAME__/Program.cs
+++ template/__APP_NAME__/Program.cs
@@ -1,6 +1,7 @@
 ﻿// This will make all the constants available in the global namespace, 
 // so you can use them without the Constants prefix.
 global using static __APP_NAME__.Constants;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace __APP_NAME__;
@@ -9,11 +10,23 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        var services = CreateServices();
         
-        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
-        ILogger<Application> logger = factory.CreateLogger<Application>();
-        
-        var app = new Application(logger);
+        var app = services.GetRequiredService<Application>();
         app.Run(args);
+    }
+    
+    private static ServiceProvider CreateServices()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.AddConsole();
+            })
+            .AddSingleton<Application>()
+            .BuildServiceProvider();
+
+        return serviceProvider;
     }
 }
